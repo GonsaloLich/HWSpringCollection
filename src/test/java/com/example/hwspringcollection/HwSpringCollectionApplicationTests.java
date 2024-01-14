e2115673a10b6adeb0a98350bcd14f8f3c48bb32
GonsaloLich <kantsaris@gmail.com>
2024-01-14 15:14:46 +0300

    Initial commit

---
+++ src/test/java/com/example/hwspringcollection/HwSpringCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwspringcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
